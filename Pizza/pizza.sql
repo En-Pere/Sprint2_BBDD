8b1ef0edd38b8d9ebac820bd0d8b0d009d6b3aec
Pere <pereruba@gmail.com>
2021-11-18 13:38:12 +0100

    update 13:38

--- Pizza/pizza.sql
+++ Pizza/pizza.sql
@@ -22,10 +22,12 @@ CREATE TABLE `provincia` (
 );
 
 CREATE TABLE `localidad` (
+    `idprovincia` int NOT NULL,
     `idlocalidad` int NOT NULL AUTO_INCREMENT,
     `localidad` VARCHAR(45) NOT NULL,
     `codigopostal` int NOT NULL,
-    PRIMARY KEY (`idlocalidad`)
+    PRIMARY KEY (`idlocalidad`),
+    FOREIGN KEY (`idprovincia`) REFERENCES `provincia`(`idprovincia`)
 );
 
 CREATE TABLE `pedidos` (
@@ -65,6 +67,98 @@ CREATE TABLE `bebidas` (
     `preciobebida` DECIMAL (5,2),
     PRIMARY KEY (`idbebidas`)
 );
+
+CREATE TABLE `pizzas` (
+    `idpizzas` int NOT NULL AUTO_INCREMENT,
+    `nombrepizza` VARCHAR(45) NOT NULL,
+    `descripcionpizza` VARCHAR(45) NOT NULL,
+    `imagenpizza` int NOT NULL,
+    `preciopizza` DECIMAL (5,2),
+    PRIMARY KEY (`idpizzas`)
+);  
+
+CREATE TABLE `hamburguesas` (
+    `idhamburguesas` int NOT NULL AUTO_INCREMENT,
+    `nombrehamburguesa` VARCHAR(45) NOT NULL,
+    `descripcionhamburguesa` VARCHAR(45) NOT NULL,
+    `imagenhamburguesa` int NOT NULL,
+    `preciohamburguesa` DECIMAL (5,2),
+    PRIMARY KEY (`idhamburguesas`)
+);  
+
+-- INSERTAR CLIENTES --
+
+INSERT INTO `pizza`.`clients` (`nom`, `cognom`, `direccioncliente`, `telefono`)
+VALUES ('antonio', 'Perez', 'C/ buenavista 42', 687554433);
+
+INSERT INTO `pizza`.`clients` (`nom`, `cognom`, `direccioncliente`, `telefono`)
+VALUES ('maria', 'gimenez', 'C/ alcantara 42', 664554433);
+
+INSERT INTO `pizza`.`clients` (`nom`, `cognom`, `direccioncliente`, `telefono`)
+VALUES ('Martina', 'Ruiz', 'Av. del carmen 42', 54654433);
+
+-- INSERTAR DATOS PROVINCIA --
+
+INSERT INTO `pizza`.`provincia` (`provincia`)
+VALUES ('Barcelona');
+
+INSERT INTO `pizza`.`provincia` (`provincia`)
+VALUES ('Tarragona');
+
+INSERT INTO `pizza`.`provincia` (`provincia`)
+VALUES ('Lleida');
+
+INSERT INTO `pizza`.`provincia` (`provincia`)
+VALUES ('Girona');
+
+-- INSERTAR DATOS LOCALIDAD --
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (1, 'Hospitalet', 08902);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (1, 'Badalona', 08911);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (2, 'Alcover', 43460);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (2, 'Alcanar', 43530);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (3, 'Agramunt', 25310);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (3, 'Aitona', 25182);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (4, 'Begur', 17255);
+
+INSERT INTO `pizza`.`localidad` (`idprovincia`, `localidad`, `codigopostal`)
+VALUES (4, 'Cabanes', 17761);
+
+-- INSERTAR DATOS PEDIDOS --
+
+INSERT INTO `pizza`.`pedidos` (`fechahora`, `entrega`)
+VALUES ('2020-07-01 00:00:00', 'tienda');
+INSERT INTO `pizza`.`pedidos` (`fechahora`, `entrega`)
+VALUES ('2020-10-10 00:00:00', 'domicilio');
+INSERT INTO `pizza`.`pedidos` (`fechahora`, `entrega`)
+VALUES ('2020-09-01 00:00:00', 'domicilio');
+INSERT INTO `pizza`.`pedidos` (`fechahora`, `entrega`)
+VALUES ('2020-06-01 00:00:00', 'tienda');
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
