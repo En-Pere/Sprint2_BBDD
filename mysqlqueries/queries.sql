1bd3979eb5c9a06f704b89b3742ea225dfe0c2f3
Pere <pereruba@gmail.com>
2021-11-23 13:36:39 +0100

    new

--- mysqlqueries/queries.sql
+++ mysqlqueries/queries.sql
@@ -5,15 +5,25 @@ SELECT nombre FROM producto
 SELECT nombre, precio FROM producto
 
 -- Llista totes les columnes de la taula producto.
-
+SELECT * FROM producto
 
 -- Llista el nom dels productos, el preu en euros i el preu en dòlars estatunidencs (USD).
+SELECT nombre, precio, precio*1.12  FROM producto
+
 -- Llista el nom dels productos, el preu en euros i el preu en dòlars estatunidencs (USD). Utilitza els següents àlies per a les columnes: nom de producto, euros, dolars.
+
+
 -- Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a majúscula.
+
+
 -- Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a minúscula.
+
 -- Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
+
 -- Llista els noms i els preus de tots els productos de la taula producto, arrodonint el valor del preu.
+
 -- Llista els noms i els preus de tots els productos de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
+
 -- Llista el codi dels fabricants que tenen productos en la taula producto.
 -- Llista el codi dels fabricants que tenen productos en la taula producto, eliminant els codis que apareixen repetits.
 -- Llista els noms dels fabricants ordenats de manera ascendent.
