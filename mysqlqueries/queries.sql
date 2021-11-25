eadcb2c40a859cb3a5e864b484fac9fcad950b89
Pere <pereruba@gmail.com>
2021-11-25 10:31:45 +0100

    update

--- mysqlqueries/queries.sql
+++ mysqlqueries/queries.sql
@@ -14,33 +14,40 @@ SELECT nombre, precio, precio*1.12  FROM producto
 SELECT nombre AS `nom de producto`, precio AS `euros`, precio*1.12 AS `dolars` FROM producto 
 
 -- Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a majúscula.
-SELECT UCASE (nombre) AS MAYUSCULA, precio FROM producto
+SELECT UPPER (nombre), precio FROM producto
 
 -- Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a minúscula.
-SELECT LCASE (nombre) AS MINUSCULA, precio FROM producto
+SELECT LOWER (nombre), precio FROM producto
 
 -- Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
-SELECT nombre, UCASE (left (nombre,2)) AS MAYUSCULA FROM fabricante
+SELECT nombre, UPPER (left (nombre,2)) FROM fabricante
 
 -- Llista els noms i els preus de tots els productos de la taula producto, arrodonint el valor del preu.
-
+SELECT nombre, ROUND(precio), precio FROM producto
 
 -- Llista els noms i els preus de tots els productos de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
+SELECT nombre, TRUNCATE(precio,0) FROM producto
 
 -- Llista el codi dels fabricants que tenen productos en la taula producto.
+SELECT codigo_fabricante FROM producto
+
 -- Llista el codi dels fabricants que tenen productos en la taula producto, eliminant els codis que apareixen repetits.
+SELECT codigo_fabricante FROM producto GROUP BY codigo_fabricante
+
 -- Llista els noms dels fabricants ordenats de manera ascendent.
+SELECT nombre FROM fabricante ORDER BY nombre ASC
+
 -- Llista els noms dels fabricants ordenats de manera descendent.
+SELECT nombre FROM fabricante ORDER BY nombre DESC
+
 -- Llista els noms dels productos ordenats en primer lloc pel nom de manera ascendent i en segon lloc pel preu de manera descendent.
+SELECT nombre FROM producto ORDER BY nombre ASC, precio DESC
+
 -- Retorna una llista amb les 5 primeres files de la taula fabricante.
+SELECT * FROM fabricante LIMIT 5
+
 -- Retorna una llista amb 2 files a partir de la quarta fila de la taula fabricante. La quarta fila també s'ha d'incloure en la resposta.
-
-
-
-
-
-
-
+SELECT * FROM fabricante LIMIT 2 OFFSET 3;
 
 -- Llista el nom i el preu del producto més barat. (Utilitzi solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podria usar MIN(preu), necessitaria GROUP BY
 -- Llista el nom i el preu del producto més car. (Utilitzi solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podria usar MAX(preu), necessitaria GROUP BY.
